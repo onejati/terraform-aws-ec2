ca4d9f2b24f6e9b262c2b565670155a0379caa9b
onejati <omidnejati.it@gmail.com>
2017-04-04 14:28:53 -0700

    Create varibles.tf

---
+++ varibles.tf
@@ -0,0 +1,15 @@
+#------------------------------------------#
+# AWS Environment Values
+#------------------------------------------#
+variable "access_key" {
+    description = "AWS account access key ID"
+}
+
+variable "secret_key" {
+    description = "AWS account secret access key"
+}
+
+variable "region" {
+  description = "EC2 Region for the VPC"
+    default = "us-west-2"
+}
